b7a0ee795c15fbba487527375e398013cac12973
Noor Abdullah Adiana <email@nooradiana.com>
2015-08-11 19:19:31 +0700

    Fix Module Convert Virtual Account

--- app/controllers/virtual/Generate_file.php
+++ app/controllers/virtual/Generate_file.php
@@ -27,30 +27,6 @@ class Generate_file extends CI_Controller
         }
     }
 
-    public function convert()
-    {
-        $this->load->model('upload_models');
-
-        if ($this->session->userdata('username') === '' || array_key_exists('username', $this->session->all_userdata()) === false) {
-            $this->load->view('errors/html/custom_error_401');
-        } else {
-            $id_file = $this->input->post('id_file');
-            $type_upload = $this->input->post('type_upload');
-
-            $record = array();
-
-            $path_file = $this->upload_models->get_file_by_id($id_file);
-
-            $file = fopen($path_file[0]['FullPathFile'],'r');
-            while(! feof($file)){
-                array_push($record, fgetcsv($file, null, ';'));
-            }
-            fclose($file);
-
-            print_r($record);
-        }
-    }
-
     public function upload_file()
     {
         $data = array(
@@ -67,7 +43,7 @@ class Generate_file extends CI_Controller
             $this->load->model('upload_models');
 
             $pilihan_bank = $this->input->post('pilihan_bank');
-            $type_upload = '';
+            $back_code = '';
 
             $config['upload_path'] = '';
             $config['allowed_types'] = 'csv';
@@ -75,11 +51,11 @@ class Generate_file extends CI_Controller
             if ($pilihan_bank == 'MDR') {
                 $config['upload_path'] = getcwd() . '/uploads/virtual/mandiri';
                 $config['file_name'] = 'MANDIRI_' . date("Ymdhis");
-                $type_upload = 'VIRTUAL ACCOUNT MANDIRI';
+                $back_code = 'MANDR';
             } elseif ($pilihan_bank == 'BRI') {
                 $config['upload_path'] = getcwd() . '/uploads/virtual/bri';
                 $config['file_name'] = 'BRIVA_' . date("Ymdhis");
-                $type_upload = 'VIRTUAL ACCOUNT BRI';
+                $back_code = 'BRIVA';
             }
 
             $this->load->library('upload', $config);
@@ -88,10 +64,35 @@ class Generate_file extends CI_Controller
                 print_r($error);
             } else {
                 $upload = array('upload_data' => $this->upload->data());
-                $insert_result = $this->upload_models->upload_file($upload['upload_data'], $this->session->userdata('username'), $type_upload);
+                $insert_result = $this->upload_models->upload_file($upload['upload_data'], $this->session->userdata('username'), $pilihan_bank);
                 if ($insert_result[0]['Status'] !== 'Failed') {
-                    $data['list_file'] = $this->upload_models->get_upload_file('', 'VIRTUAL');
-                    $this->load->view('virtual/generate_file', $data);
+
+                    $this->load->model('virtualaccount_models');
+                    $record = array();
+                    $path_file = $upload['upload_data']['full_path'];
+                    $file_name = $upload['upload_data']['file_name'];
+
+                    $file = fopen($path_file, 'r');
+                    while (!feof($file)) {
+                        if($pilihan_bank == 'BRI'){
+                            array_push($record, fgetcsv($file, null, ';'));
+                        }elseif($pilihan_bank == 'MDR'){
+                            array_push($record, fgetcsv($file, null, ','));
+                        }
+                    }
+                    fclose($file);
+
+                    $insert_record = $this->virtualaccount_models->insert_virtualaccount($record, $back_code, $this->session->userdata('username'), $file_name);
+
+                    if($insert_record['Status'] !== 'Failed'){
+                        $data['list_file'] = $this->upload_models->get_upload_file('', 'VIRTUAL');
+                        $this->load->view('virtual/generate_file', $data);
+                    }else{
+                        nlink($upload['upload_data']['full_path']);
+                        $data['status'] = $insert_result[0]['Status'];
+                        $data['pesan'] = $insert_result[0]['Pesan'];
+                        $this->load->view('virtual/generate_file', $data);
+                    }
                 } else {
                     unlink($upload['upload_data']['full_path']);
                     $data['status'] = $insert_result[0]['Status'];
@@ -99,6 +100,37 @@ class Generate_file extends CI_Controller
                     $this->load->view('virtual/generate_file', $data);
                 }
             }
+        }
+    }
+
+    public function convert()
+    {
+        date_default_timezone_set("Asia/Jakarta");
+        $this->load->model('upload_models');
+        $this->load->model('virtualaccount_models');
+
+        $id_file = $this->input->post('id_file');
+        $type_upload = $this->input->post('type_upload');
+        if ($this->session->userdata('username') === '' || array_key_exists('username', $this->session->all_userdata()) === false) {
+            $this->load->view('errors/html/custom_error_401');
+        } else {
+            $path_file = $this->upload_models->get_file_by_id($id_file);
+            $file_name = $path_file[0]['NamaFile'];
+            $data_file = $this->virtualaccount_models->get_va_by_file($file_name, $this->session->userdata('username'));
+            $file_download_path = '';
+
+            if($type_upload == 'MDR'){
+                $file_download_path = getcwd() . '/downloads/virtual/mandiri/CONVERT_MANDIRI_'.date("Ymdhis").'.txt';
+            }else{
+                $file_download_path = getcwd() . '/downloads/virtual/bri/CONVERT_BRIVA_'.date("Ymdhis").'.txt';
+            }
+
+            $file_download = fopen($file_download_path, 'w');
+            for($i = 0; $i < sizeof($data_file); $i++){
+                fwrite($file_download, $data_file[$i]['ConvertData']."\r\n");
+            }
+            fclose($file_download_path);
+            force_download($file_download_path, NULL);
         }
     }
 }
--- app/models/Virtualaccount_models.php
+++ app/models/Virtualaccount_models.php
@@ -3,7 +3,58 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Virtualaccount_models extends CI_Model
 {
-    function insert_virtualaccount($virtualaccount = array(), $bank_code){
+    function insert_virtualaccount($virtualaccount = array(), $bank_code, $user, $file_name)
+    {
+        $ITCORPDB = $this->load->database('ITCORPDB', TRUE);
 
+        $ITCORPDB->trans_begin();
+        $ITCORPDB->query('SET ANSI_NULLS ON;');
+        $ITCORPDB->query('SET ANSI_WARNINGS ON;');
+        for($i = 1; $i < (sizeof($virtualaccount)-1); $i++){
+            if($bank_code == 'BRIVA'){
+                $query_string = "EXEC spConvertVirtualAccountBRI @NomorUrut = '".$virtualaccount[$i]['1']."',
+								@TanggalTransaksi  = '".$virtualaccount[$i]['2']."',
+								@CustomerCode = '".$virtualaccount[$i]['3']."',
+								@TotalPembayaran = '".$virtualaccount[$i]['5']."',
+								@BankCode = '".$bank_code."',
+								@FileName = '".$file_name."',
+								@UserInsert = '".$user."'";
+            }elseif($bank_code == 'MANDR'){
+                $query_string = "EXEC spConvertVirtualAccountMandiri @NomorUrut = '".$i."',
+								@CustomerID  = '".$virtualaccount[$i]['8']."',
+								@TanggalTransaksi = '".$virtualaccount[$i]['0']."',
+								@TotalPembayaran = '".$virtualaccount[$i]['4']."',
+								@BankCode = '".$bank_code."',
+								@FileName = '".$file_name."',
+								@UserInsert = '".$user."'";
+            }
+
+            $ITCORPDB->query($query_string);
+        }
+        if ($this->db->trans_status() === FALSE){
+            $this->db->trans_rollback();
+            return array(
+                'Status' => 'Failed',
+                'Pesan' => 'Error insert virtual account'
+            );
+        }else{
+            $this->db->trans_commit();
+            return array(
+                'Status' => 'Success',
+                'Pesan' => 'Insert virtual account berhasil'
+            );
+        }
+
+    }
+
+    function get_va_by_file($file_name, $user)
+    {
+        $ITCORPDB = $this->load->database('ITCORPDB', TRUE);
+        $query_string = "EXEC spGetConvertVirtualAccount @FileName = '".$file_name."', @UserConvert = '".$user."'";
+
+        $ITCORPDB->query('SET ANSI_NULLS ON;');
+        $ITCORPDB->query('SET ANSI_WARNINGS ON;');
+        $query = $ITCORPDB->query($query_string);
+        return $query->result_array();
     }
 }
